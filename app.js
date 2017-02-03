f6c133bf80ae24fcbd458b35e1c8ea76fd57d287
Rémi Mourguiart <Rmi Mourguiart>
2017-02-03 16:03:26 +0100

    Start TP3

--- app.js
+++ app.js
@@ -13,12 +13,75 @@ app.set( 'view engine', 'ejs' );
 app.use( '/assets', express.static( 'assets' ) );
 
 
+/*
+{
+	"title": "Example Schema",
+	"type": "object",
+	"properties": {
+		"firstName": {
+			"type": "string"
+		},
+		"lastName": {
+			"type": "string"
+		},
+		"age": {
+			"description": "Age in years",
+			"type": "integer",
+			"minimum": 0
+		}
+	},
+	"required": ["firstName", "lastName"]
+}*/
+
+
+
+function callLeboncoin() {
+    var url = 'https://www.leboncoin.fr/ventes_immobilieres/968817200.htm?ca=12_s'
+
+    request( url, function ( error, response, html ) {
+        if ( !error && response.statusCode == 200 ) {
+            const $ = cheerio.load( html )
+
+            const lbcDataArray = $( 'section.properties span.value' )
+
+            let lbcData = {
+                price: parseInt( $( lbcDataArray.get( 0 ) ).text().replace( /\s/g, '' ), 10 ),
+                city: $( lbcDataArray.get( 1 ) ).text().trim().toLowerCase().replace( /\_|\s/g, '-' ),
+                type: $( lbcDataArray.get( 2 ) ).text().trim().toLowerCase(),
+                surface: parseInt( $( lbcDataArray.get( 4 ) ).text().replace( /\s/g, '' ), 10 )
+                //squarePrice: price / surface
+            }
+            console.log( lbcData )
+            /*res.render( 'home', {
+                message2: price,
+                message3: city,
+                message4: type,
+                message5: surface
+            })*/
+        }
+        else {
+            console.log( error )
+        }
+    })
+}
+
+
+
+
 //makes the server respond to the '/' route and serving the 'home.ejs' template in the 'views' directory
-/*app.get( '/', function ( req, res ) {
+app.get( '/', function ( req, res ) {
+
+
+    var url = req.query.urlLBC
+
+    //console.log( req.query )
+    callLeboncoin();
+
     res.render( 'home', {
-        message: 'The Home Page!'
+        message: url
+
     });
-});*/
+});
 
 
 //launch the server on the 3000 port
@@ -27,41 +90,36 @@ app.listen( 3000, function () {
 });
 
 
+function callLeMeilleurAgent() {
+    var url = 'https://www.meilleursagents.com/prix-immobilier/paris-75000/'
 
-// Affichage de la Page
-app.get( '/', function ( req, res ) {
-    request( 'https://www.leboncoin.fr/ventes_immobilieres/1076257949.htm?ca=12_s', function ( error, response, body ) {
-        if ( !error & response.statusCode == 200 ) {
-            console.log( body ) //Show the html
+    request( url, function ( error, response, html ) {
+        if ( !error && response.statusCode == 200 ) {
+            const $ = cheerio.load( html )
 
-            var $ = cheerio.load( htlm );
-            var price, surface, city, type;
-            var json = { price: "", surface: "", city: "", type: "" };
-            var data = console.log( $( "section.properties span.values" ) )
-            price = data.children().first().text();
-            console.log( price );
-            city = data.children().next().text();
-            console.log( city );
-            type = data.children().next().text();
-            surface = data.children().next().next().text();
+            const lmaDataArray = $( 'section.properties span.value' )
 
+            let lmaData = {
+                meanPriceHouse: parseInt( $( lmaDataArray.get( 0 ) ).text().replace( /\s/g, '' ), 10 ),
+                meanPriceAppart: parseInt( $( lmaDataArray.get( 1 ) ).text().trim().toLowerCase().replace( /\_|\s/g, '-' ) ),
+                meanPriceRent: parseInt( $( lmaDataArray.get( 2 ) ).text().trim().toLowerCase() ),
+            }
+            console.log( lmaData )
         }
-        res.send( body )
+        else {
+            console.log( error )
+        }
     })
-});
+}
 
-/*
-function recuperation( body ) {
-    if ( !error ) {
-        var $ = cheerio.load( htlm );
-        var price, surface, city, type;
-        var json = { price: "", surface: "", city: "", type: "" };
-        var data = console.log( $( "section.properties span.values" ) )
-        price = data.children().first().text();
-        console.log( price );
-        city = data.children().next().text();
-        console.log( city );
-        type = data.children().next().text();
-        surface = data.children().next().next().text();
-    }
-}*/
+
+/*function calllemeilleuragent() {
+    var url2 = 'https://www.meilleursagents.com/prix-immobilier/'
+    request( url2, lbcData.city.toLowerCase() + '-' + lbcData.codePostal, function ( error, response, body ) {
+                if ( !error && response.statusCode == 200 ) {
+}   
+*/
+
+
+
+
